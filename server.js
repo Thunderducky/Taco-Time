de84a0802c460d16088a1cbda715233ee1e82a22
Matthew Kinzle <matt.kinzle@gmail.com>
2020-07-22 20:32:21 -0500

    making moves

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const mysql = require("mysql");
-const escapeHtml = require("escape-html");
+// const escapeHtml = require("escape-html");
 const connection = require("./config/connection")
-const express = require("express");
 const view = require("./views");
 const app = express();
 
@@ -12,21 +11,24 @@ const PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+//allows access to public folder
+app.use(express.static("public"));
+
 //routes
 app.get("/", function (req, res) {
     res.send(view.app(
         `
-      <h1>Index</h1>
-      <a href="/tacos">Take me to the Taco Heaven!</a>
+		<li class="list-group-item">${taco.taco_name}</li>
+		<center><button data-devour="${taco.id}" class="btn devour-it" type="submit">Down the Hatch!</button></center>
     `
     ));
 })
 
-app.post("/", function (req, res) {
-    const taco = req.body.taco
+// app.post("/", function (req, res) {
+//     const taco = req.body.taco
 
-    connection.query("INSERT INTO tacos (taco) VALUES (?)",)
-})
+//     connection.query("INSERT INTO tacos (taco) VALUES (?)",)
+// })
 
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
--- views/tacos.js
+++ views/tacos.js
@@ -1,20 +1,10 @@
 module.exports = function (props) {
-    return `
-    <h1>My Tacos</h1>
-
-	<h3>Here lies the top of the taco heap!</h3>
-	
-	<ul>
-	${
-        props.tacos.map(taco => {
-            return `
-			<li>
-				<p>ID: ${taco.id}</p>
-				<p>Task: ${taco.task}</p>
-			</li>
-			`
-        }).join("")
-        }
-	</ul>
-	    `
+	return `
+	${tacos.map(taco => {
+		return `
+		<li class="list-group-item">${taco.taco_name}</li>
+		<center><button data-devour="${taco.id}" class="btn devour-it" type="submit">Down the Hatch!</button></center>
+		`
+	}).join("\n")}
+`
 }
